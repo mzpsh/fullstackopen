88dd65beaef323570a13deebc0979f2a9d09f7ea
mzpsh <mirzamiftahulfadillah@gmail.com>
2024-06-20 21:10:49 +0800

    syncing

---
+++ part1/courseinfo/src/App.jsx
@@ -0,0 +1,68 @@
+const Header = (props) => {
+  return (
+    <div>
+      <h1>{props.course.name}</h1>
+    </div>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <>
+      <p>{props.part} {props.exercise}</p>
+    </>
+  )
+}
+
+const Content = (props) => {
+  const { parts } = props.course
+ return (
+    <div>
+      <Part part={parts[0].name} exercise={parts[0].exercises}/>
+      <Part part={parts[1].name} exercise={parts[1].exercises}/>
+      <Part part={parts[2].name} exercise={parts[2].exercises}/>
+    </div>
+ )
+}
+
+const Total = (props) => {
+  const { parts } = props.course;
+
+  const total = parts[0].exercises +  parts[1].exercises + parts[2].exercises
+  return (
+    <div>
+      <p>Number of exercises {total}</p>
+    </div>
+  )
+}
+
+const App = () => {
+
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <Header course={course}/>
+      <Content course={course}/>
+      <Total course={course}/> 
+    </div>
+  )
+}
+
+export default App
