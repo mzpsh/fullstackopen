db04baa3f2a104acb6326ea3917c8e69d0ca72ce
mzpsh <mirzamiftahulfadillah@gmail.com>
2024-08-19 22:25:51 +0800

    modified phonebook for 3.11

--- part2/phonebook/vite.config.js
+++ part2/phonebook/vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://fullstackopen-part3-patient-glitter-555.fly.dev/',
+        changeOrigin: true,
+      }
+    }
+  }
 })
