918bef4fdba0c5333352b966b3cd8dbf25b4c1ac
mzpsh <mirzamiftahulfadillah@gmail.com>
2025-10-05 14:45:57 +0800

    preparing practices for 5.1

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -50,11 +50,17 @@ const App = () => {
     setUser(null)
   }
 
+  const [newTitle, setNewTitle] = useState('')
+  const [newAuthor, setNewAuthor] = useState('')
+  const [newUrl, setNewUrl] = useState('')
+
   return (
     user != null
       ? <>
         <h2>blogs</h2>
         <p>{user['name']} is logged in <button onClick = {handleSignOut}>sign out</button></p>
+        <h2>create new</h2>
+
         {blogs.map(blog =>
           <Blog key = {blog.id}
             blog = {blog} />
